759aec04816fdcc917ef4153d7d6bfd76eb2991a
genezhangzhendong <genezhangzhendong@didiglobal.com>
2021-12-19 19:50:19 +0800

    fix: unit test modified

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/didi/waitInHopeRepo/mywebpack/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/didi/waitInHopeRepo/mywebpack/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js' > -1), true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1,  true);
   });
 });
