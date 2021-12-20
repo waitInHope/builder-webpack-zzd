05b94484ddb8d6b1f5faa6ab7c109a75919eaa50
genezhangzhendong <genezhangzhendong@didiglobal.com>
2021-12-20 18:42:29 +0800

    fix: 运行单元测试，看是否触发CI缓存设置

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 process.chdir(path.join(__dirname, 'smoke/template'));
 
+// 运行单元测试
 describe('build webpack test case', () => {
   require('./unit/webpack-base-test');
 });
